249d631e1d437ac6dbbf12a5ac3c032cc0f5027e
Bogdan Pankiv <bogdan@fabrica.net.ua>
2015-02-08 00:07:43 +0200

    first plot created

---
+++ plot1.R
@@ -0,0 +1,8 @@
+x <- read.csv2("household_power_consumption.txt", header=TRUE, colClasses=c(rep("character",9)), na.strings="?")
+x[,1] <- as.Date(x[,1], "%d/%m/%Y")
+y <- x[x$Date == as.Date("2007-02-02") | x$Date == as.Date("2007-02-01"),]
+y[,3] <- as.numeric(y[,3])
+
+png("plot1.png")
+hist(y$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
