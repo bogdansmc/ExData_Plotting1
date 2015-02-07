ce709cf5ebcb13368914664e819a06d6371fbb4c
Bogdan Pankiv <bogdan@fabrica.net.ua>
2015-02-08 00:41:00 +0200

    third plot created

---
+++ plot3.R
@@ -0,0 +1,13 @@
+x <- read.csv2("household_power_consumption.txt", header=TRUE, colClasses=c(rep("character",9)), na.strings="?")
+x[,1] <- as.Date(x[,1], "%d/%m/%Y")
+y <- x[x$Date == as.Date("2007-02-02") | x$Date == as.Date("2007-02-01"),]
+y[,3] <- as.numeric(y[,3])
+y$DateTime <- strptime(paste(y[,1], y[,2]), "%Y-%m-%d %H:%M:%OS")
+
+png("plot3.png")
+plot(y$DateTime, y$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(y$DateTime, y$Sub_metering_2, col="red")
+lines(y$DateTime, y$Sub_metering_3, col="blue")
+
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
